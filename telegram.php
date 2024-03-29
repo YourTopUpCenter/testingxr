b378116691ebeae7015d5808bf674e7eb20761f8
YourTopUpCenter <139861458+YourTopUpCenter@users.noreply.github.com>
2024-03-29 12:34:57 +0600

    vuuu

--- telegram.php
+++ telegram.php
@@ -1,13 +1,17 @@
+<form action="" method="post">
+    <input type="text" name="message">
+    <input type="submit" name="submit">
+</form>
+
+
 <?php
-
-$apiToken = "7058391968:AAGZaNkOjzzXClA1QT3l0QzIHfSkLuo-Ijg";
-
-$data = [
-    'chat_id' => '@webapitesting_error575', //-1001934360723
-    'text' => 'Hello world!'
-];
-
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
-// Do what you want with result
-
+    if(isset($_POST['submit']))
+    {
+        $apiToken = "7058391968:AAGZaNkOjzzXClA1QT3l0QzIHfSkLuo-Ijg";
+        $data = [
+            'chat_id' => '@webapitesting_error575', 
+            'text' => $_POST['message']
+        ];
+        $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );    
+    }
 ?>
